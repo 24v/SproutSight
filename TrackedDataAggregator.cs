38d4b492aa0be05588b869789d6181335d010bc0
24v <github@bayonetwork.com>
2025-03-15 18:26:24 -0700

    Fucking ordering on this shit

--- TrackedDataAggregator.cs
+++ TrackedDataAggregator.cs
@@ -23,34 +23,22 @@ internal class TrackedDataAggregator(TrackedData TrackedData, Operation Operatio
 
     public void LoadAggregationAndViewVisitor()
     {
-        Logging.Monitor.Log("Starting LoadAggregationAndViewVisitor", LogLevel.Debug);
 
         // Create the year data structure
         List<YearNode> yearNodes = [];
         RootNode rootNode = new(yearNodes);
-        // if (SelectedYears.Length == 0)
-        // {
-        //     Logging.Monitor.Log("No years selected, returning early", LogLevel.Error);
-        //     return;
-        // }
-        
-        Logging.Monitor.Log($"SelectedYears: [{string.Join(", ", SelectedYears)}], Game1.year: {Game1.year}", LogLevel.Error);
-        
+
         int[] yearsToFill;
         if (SelectedYears.Contains(0))
         {
-            Logging.Monitor.Log("All years selected, filling with all years", LogLevel.Error);
-            // Create a properly sized array
             yearsToFill = new int[Game1.year];
             for (int i = 0; i < Game1.year ; i++)
             {
                 yearsToFill[i] = i + 1;
-                Logging.Monitor.Log($"Added year {i + 1} at index {i}", LogLevel.Error);
             }
         } 
         else 
         {
-            Logging.Monitor.Log($"Using specific years: [{string.Join(", ", SelectedYears)}]", LogLevel.Error);
             yearsToFill = SelectedYears.Reverse().ToArray();
         }
         
